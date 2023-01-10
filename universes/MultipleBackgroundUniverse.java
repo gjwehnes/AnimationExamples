8056f5bd5c0f5a6b774bef55a6d45711dea4c704
gjwehnes <gjwehnes>
2023-01-09 20:55:14 -0700

    avoid use of Double.MINX for logical coordinates; seems to cause an issue with translation to screen coordinates

--- universes/MultipleBackgroundUniverse.java
+++ universes/MultipleBackgroundUniverse.java
@@ -27,7 +27,7 @@ public class MultipleBackgroundUniverse implements Universe {
 		this.setYCenter(0);
 		player1 = new JumpingSprite(0, -250);
 		sprites.add(player1);
-		sprites.add(new BarrierSprite(Double.MIN_VALUE, 0, Double.MAX_VALUE, 500, false));
+		sprites.add(new BarrierSprite(-1000000, 0, 1000000, 500, true));
 			
 	}
 
