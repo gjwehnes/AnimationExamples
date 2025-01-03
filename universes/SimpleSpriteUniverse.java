4b949a4756e05121190e4eabe345864fe22b5a2f
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-01-03 12:51:21 -0700

    calculateStandardAngle moved to TrigonometryUtility class

--- universes/SimpleSpriteUniverse.java
+++ universes/SimpleSpriteUniverse.java
@@ -91,69 +91,11 @@ public class SimpleSpriteUniverse implements Universe {
     	} 
 
 	}
-
-	private double calculateStandardAngle(double xA, double yA, double xB, double yB) {
-		
-		// An example of how to calculate standard angles in java that corresponds to the
-		// mathematical definition. This is somewhat complicated because, opposite to a Cartesian grid
-		// the y dimension increases towards the bottom of the screen. Thus, our 'standard'
-		// angle is reversed from the mathematical definition. 0 is still to the right,
-		// but positive angles indicate clockwise rotation. 90 is the bottom of the screen,
-		// 180 is the left, and 270 is the top. That allows the sine, cosine, and tangent ratios
-		// still correctly correspond to the standard angle
-		
-		// Thus, quadrants are reversed. Top right is I, bottom right is II,
-		// bottom left is III, top left is IV
-
-		// As a further complication, methods in the Math class work with radians, which
-		// are not necessarily intuitive. So, the examples below convert to radians when
-		// necessary, but we store the angle in degrees.
-		
-		double x = xB - xA;
-		double y = yB - yA;
-		
-		// calculate tan ratio
-		double tan = y / x;
-		// calculate reference angle (thetaR)
-		// note the conversion from radians to degrees
-		// reference angle is always positive; the Math.abs method removes the sign.
-		double thetaR = Math.abs( Math.toDegrees(Math.atan(tan)));
-		
-		//based on the quadrant, reference angle will give us standard angle (theta)
-		double theta;
-
-		if (x >= 0 && y >= 0) {
-			//quadrant I
-			theta = thetaR;			
-		} else if (x < 0 && y >= 0) {
-			//quadrant II
-			theta = 180 - thetaR;		
-		} else if (x < 0 && y < 0) {
-			//quadrant III
-			theta = 180 + thetaR;		
-		} else {
-			//quadrant IV
-			theta = 360 - thetaR;		
-		}
-		
-		return theta;
-		
-		/*
-		 * Conversion from standard angle back to vectors (i.e. the corresponding x and distance)
-		 * using the sine and cosine function will still work correctly. See the SpaceshipSprite
-		 * for an example
-		 */
-	}
 	
 	public String toString() {		
-		return String.format("mouseX: %3.2f; mouseY: %3.2f; standard angle: %5.1f; buttons: %s%s",
+		return String.format("mouseX: %3.2f; mouseY: %3.2f; buttons: %s%s",
 				MouseInput.logicalX,
 				MouseInput.logicalY,
-				calculateStandardAngle(
-						this.player1.getCenterX(),
-						this.player1.getCenterY(),
-						MouseInput.logicalX,
-						MouseInput.logicalY),
 				MouseInput.leftButtonDown ? "X" : "-",
 				MouseInput.rightButtonDown ? "X" : "-");
 	}
