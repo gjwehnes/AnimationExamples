9f01d7fc83a7c51977ef42c3450e05cb513dcf46
gjwehnes <gjwehnes@E829WL240006.org.cbe.ab.ca>
2025-01-05 12:40:54 -0700

    added trigonometry examples to SimpleSpriteUniverse; removed grid

--- gui/AnimationExamplesFrame.java
+++ gui/AnimationExamplesFrame.java
@@ -21,7 +21,7 @@ public class AnimationExamplesFrame extends AnimationFrame{
 	JComboBox cboUniverse;	
 
 	public AnimationExamplesFrame(Animation animation) {
-		
+
 		super(animation);
 
 		cboUniverse  = new JComboBox();
@@ -44,11 +44,11 @@ public class AnimationExamplesFrame extends AnimationFrame{
 				cboUniverse_itemStateChanged(e);
 			}
 		});			
-				
+
 	}
-	
+
 	private void cboUniverse_itemStateChanged(ItemEvent e) {
-	
+
 		/*
 		 * In Java Swing, you have to be very careful with events, as one
 		 * user action can trigger multiple events. In this case, changing
@@ -56,7 +56,7 @@ public class AnimationExamplesFrame extends AnimationFrame{
 		 * SELECTED event
 		 */
 		if (e.getStateChange() == ItemEvent.SELECTED) {
-					
+
 			if (this.cboUniverse.getSelectedIndex() == 0) {
 				this.animation.switchUniverse(SimpleSpriteUniverse.class.toString());		
 			}
@@ -86,12 +86,70 @@ public class AnimationExamplesFrame extends AnimationFrame{
 			}
 			else {			
 			}
-			
+
 			this.universe = animation.getCurrentUniverse();
 			this.sprites = universe.getSprites();
 			this.backgrounds = universe.getBackgrounds();
 			this.scale = universe.getScale();
 		}
 	}
-		
+
+	protected void paintAnimationPanel(Graphics g) {
+
+		/*
+		 * An example of how to draw other graphical elements (lines, text) on the screen. This is here done with
+		 * the MouseInput but you can also easily do so using the location of a given sprite.
+		 * 
+		 * This is also an example of how you can extend the functionality of the AnimationFrame without modifying
+		 * that class directly. As many projects in the Animation code base rely on the AnimationFrame, we may not want
+		 * to add functionality specific to one project in what is intended to be a generic class. 
+		 */
+		if (MouseInput.rightButtonDown
+				&& translateToScreenX(MouseInput.logicalX) > 0
+				&& translateToScreenY(MouseInput.logicalY) > 0) {
+
+			Graphics2D g2 = (Graphics2D) g;
+			g2.setColor(Color.GREEN);
+			g.setFont(new Font(null, 0, 12));
+
+			double angle = TrigonometryUtility.calculateStandardAngle(
+					logicalCenterX,
+					logicalCenterY,
+					MouseInput.logicalX,
+					MouseInput.logicalY);
+			
+			
+			
+			int screenMidpointX = translateToScreenX((logicalCenterX + MouseInput.logicalX)/2);
+			int screenMidpointY = translateToScreenY((logicalCenterY + MouseInput.logicalY)/2);
+
+		     /*
+		      * Draw a line from the logical center to the current location of the mouse pointer. Note that both
+		      * of these are accessible as logical coordinates, so they need to be translated to screen coordinates
+		      */
+
+			g2.setColor(Color.GREEN);
+			g2.setStroke(new BasicStroke(2));
+		    g2.drawLine(translateToScreenX(logicalCenterX),
+		    		translateToScreenY(logicalCenterY),
+		    		translateToScreenX(MouseInput.logicalX),
+		    		translateToScreenY(MouseInput.logicalY));				
+			
+		    /*
+		     * Add the angle as a text object, at the end point of a line segment that is perpendicular
+		     * to the first line, extending from the midpoint, and with length 25. This simply ensures that
+		     * the text is not over top of the first line, which would make it difficult to read.
+		     * Observe that we can simply add 90° to the angle. The java cosine and sine functions can work
+		     * with any angle (i.e negative angles or those > 360°), but remember to convert to radians first.
+		     */
+			int offsetX = (int) (25 * Math.cos(Math.toRadians(angle + 90)));
+			int offsetY = (int) (25 * Math.sin(Math.toRadians(angle + 90)));
+										
+			g2.drawString(String.format("%3.2f°", angle),
+					screenMidpointX + offsetX,
+					screenMidpointY + offsetY);
+			
+		}
+	}	
+
 }
--- universes/SimpleSpriteUniverse.java
+++ universes/SimpleSpriteUniverse.java
@@ -17,13 +17,11 @@ public class SimpleSpriteUniverse implements Universe {
 		 */
 		
 		player1 = new SimpleSprite(0,0);
-		ImageSprite background = new ImageSprite(-407, -307, 410, 308, "res/backgrounds/grid.jpg");
 
 		/*
 		 * The order in which sprites are added to the sprites list matters. Sprites added earlier will have a lower
 		 * index and will be rendered earlier. Sprites added later will thus appear 'in front' of sprites added earlier.
 		 */
-		sprites.add(background);
 		sprites.add(player1);
 		
 	}
